b6319ad05f8fd331d57dab8486f18f076619e7ce
xgll7 <gouxiaosong1001@gmail.com>
2026-03-08 21:50:25 +0800

    feat: add phpstan

--- src/Core/Payment/Payment.php
+++ src/Core/Payment/Payment.php
@@ -2,8 +2,12 @@
 
 namespace Shopwell\Core\Payment;
 
+use Payum\Bundle\PayumBundle\PayumBundle;
 use Shopwell\Core\Framework\Bundle;
+use Shopwell\Core\Framework\Parameter\AdditionalBundleParameters;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
 /**
  * @internal
@@ -14,5 +18,15 @@ class Payment extends Bundle
     {
         parent::build($container);
         $this->buildDefaultConfig($container);
+
+        $phpLoader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/DependencyInjection/'));
+        $phpLoader->load('payum.php');
+    }
+
+    public function getAdditionalBundles(AdditionalBundleParameters $parameters): array
+    {
+        return [
+            new PayumBundle(),
+        ];
     }
 }
